9ffd2619675224923a7fbc7591fdad895b4ec068
landinguy <1029573528@qq.com>
2020-12-31 18:08:19 +0800

    update

--- test.py
+++ test.py
@@ -62,25 +62,13 @@ def paint():
     screen.mainloop()
 
 
-def test_request():
-    # url = 'http://www.baidu.com'
-    # url = 'https://www.amazon.com/s/query?k=电脑&page=2&qid=1609216438&ref=sr_pg_1'
-    head = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'}
-    cookie = {
-        'session-id': '137-2269336-8203106',
-        'session-id-time': '2082787201l',
-        'csm-hit': 'tb:1AQZE02WRB3JT40EMKRT+s-S73NP6XHSXTYFJC3QM3Y|1609212123210&t:1609212123210&adb:adblk_no',
-        'ubid-main': '134-3828777-8944234',
-        'session-token': 'TW2YyjF9rmjY3rLXsyBELZrmcAgDds0xOWZl6fo7KyDEPlNV/JTmLj0JODu0UpGwKJiyJ2hOq7zGqEytdl8mXijb2fUV/Ds5R2bzO43cJs/FB2CXecxZTaU6UbdS7a1YmRp3FesTaB/EJjpKIGo6XtcMM1WvMF2KnuG2W/XLmuj+PuEjBCnzZPYqROWhFBzg',
-        'lc-main': 'en_US',
-        'skin': 'noskin',
-    }
-    url = 'https://www.amazon.com/s?k=电脑&page=3'
-    r = requests.get(url, headers=head, cookies=cookie)
+# 爬取亚马逊数据
+def get_amazon_data(cookies, keyword, path, page=1):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'}
+    url = f'https://www.amazon.com/s?k={keyword}&page={page}'
+    r = requests.get(url, headers=headers, cookies=cookies)
     r.encoding = 'utf-8'
-    # log.info('url#%s' % r.url)
-    # log.info('text#%s' % r.text)
-    with open('C:\\Users\\xiaoxiaofei\\Desktop\\12293.txt', 'w+', encoding='utf8') as f:
+    with open(path, 'w+', encoding='utf8') as f:
         f.write(r.text)
 
 
@@ -96,5 +84,16 @@ def download(url, path='d:/'):
 
 
 if __name__ == '__main__':
-    test_request()
+    cookies = {
+        'session-id': '137-2269336-8203106',
+        'session-id-time': '2082787201l',
+        'csm-hit': 'tb:1AQZE02WRB3JT40EMKRT+s-S73NP6XHSXTYFJC3QM3Y|1609212123210&t:1609212123210&adb:adblk_no',
+        'ubid-main': '134-3828777-8944234',
+        'session-token': 'TW2YyjF9rmjY3rLXsyBELZrmcAgDds0xOWZl6fo7KyDEPlNV/JTmLj0JODu0UpGwKJiyJ2hOq7zGqEytdl8mXijb2fUV/Ds5R2bzO43cJs/FB2CXecxZTaU6UbdS7a1YmRp3FesTaB/EJjpKIGo6XtcMM1WvMF2KnuG2W/XLmuj+PuEjBCnzZPYqROWhFBzg',
+        'lc-main': 'en_US',
+        'skin': 'noskin',
+    }
+    filename = '123101.txt'
+    filepath = f'C:\\Users\\xiaoxiaofei\\Desktop\\{filename}'
+    get_amazon_data(cookies, '电脑', filepath)
     # pass
